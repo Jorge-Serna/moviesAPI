e715ef8fcc77bf3c2113855916299a439dd20a59
Jorge-Serna <jorge.alfonso.serna@gmail.com>
2026-01-03 09:54:28 -0600

    DB connection was set. Data base migration done

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using moviesAPI.Models;
+namespace moviesAPI.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
